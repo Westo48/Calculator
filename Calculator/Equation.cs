a6f1d80771eaf0f793650e0d33a5c7d32906529e
Westo48 <46290484+Westo48@users.noreply.github.com>
2019-10-26 14:05:34 -0500

    Initial commit

---
+++ Calculator/Equation.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Equation
+    {
+        public Equation()
+        {
+            nums = new List<decimal>();
+            ops = new List<char>();
+        }
+
+        // * nums and ops list methods
+        public string NumClick(string clickedNum)
+        {
+
+            isSolvedNum = false;
+            if (currentNum == "0")
+            {
+                currentNum = clickedNum;
+                return currentNum;
+            }
+            else
+            {
+                currentNum += clickedNum;
+                return currentNum;
+            }
+        }
+        public string DecClick()
+        {
+            CheckSolvedNum();
+            // checks if the currentNum is blank, 0, only negative, or is already a decimal
+            if (currentNum == "" || currentNum == "0")
+            {
+                currentNum = "0.";
+            }
+            else if (currentNum == "-")
+            {
+                currentNum = "-0.";
+            }
+            else if (!currentNum.Contains("."))
+            {
+                currentNum += ".";
+            }
+            return currentNum;
+        }
+
+        public void OpClick(char clickedOp)
+        {
+            // logic that works if Solve has just run and user clicks an operator to operate on the answer directly after
+            //cannot use CheckSolvedNum here due to different logic
+            if (isSolvedNum)
+            {
+                nums.Add(answerNum);
+                ops.Add(clickedOp);
+                isSolvedNum = false;
+            }
+            else
+            {
+                // checks if currentNum has a value or not, then checks to see if ops has a value or not
+                if (currentNum == "")
+                {
+                    if (ops.Count == 0)
+                    {
+                        ops.Add(clickedOp);
+                        currentNum = "0";
+                        AddNewNum();
+                    }
+                    else
+                    {
+                        ops.RemoveAt(ops.Count - 1);
+                        ops.Add(clickedOp);
+                    }
+                }
+                else if (currentNum == "-")
+                {
+                    currentNum = "0";
+                    ops.Add(clickedOp);
+                    AddNewNum();
+                }
+                else
+                {
+                    lastNum = Convert.ToDecimal(currentNum);
+                    ops.Add(clickedOp);
+                    AddNewNum();
+                }
+                lastOp = clickedOp;
+            }
+        }
+        public string BackClick()
+        {
+            CheckSolvedNum();
+            if (currentNum == "")
+            {
+                return "0";
+            }
+            // this includes if currentNum="-"
+            else if (currentNum.Length == 1)
+            {
+                currentNum = currentNum.Remove(currentNum.Length - 1, 1);
+                return "0";
+            }
+            else
+            {
+                currentNum = currentNum.Remove(currentNum.Length - 1, 1);
+                return currentNum;
+            }
+
+        }
+        public string ClearClick()
+        {
+            isSolvedNum = false;
+            undefined = false;
+            currentNum = "";
+            return "0";
+        }
+        public string ClearAllClick()
+        {
+            isSolvedNum = false;
+            undefined = false;
+            currentNum = "";
+            ops.Clear();
+            nums.Clear();
+            return "0";
+        }
+        public string PosNegClick()
+        {
+            CheckSolvedNum();
+            if (currentNum == "")
+            {
+                currentNum = "-";
+            }
+            // checking for negativity and inverting said negativity
+            else if (currentNum.StartsWith("-"))
+            {
+                currentNum = currentNum.Remove(0, 1);
+            }
+            else
+            {
+                currentNum = $"-{currentNum}";
+            }
+            return currentNum;
+        }
+
+        // * list modification and check methods
+        public void AddNewNum()
+        {
+            nums.Add(Convert.ToDecimal(currentNum));
+            currentNum = "";
+        }
+        //checks if isSolvedNum value is true
+        public void CheckSolvedNum()
+        {
+            if (isSolvedNum)
+            {
+                isSolvedNum = false;
+                currentNum = Convert.ToString(answerNum);
+            }
+        }
+
+        // * math operations
+        public string Solve()
+        {
+            if (isSolvedNum)
+            {
+                nums.Add(answerNum);
+                nums.Add(lastNum);
+                ops.Add(lastOp);
+                SolveExponent();
+                SolveMultiply();
+                SolveDivide();
+                if (undefined)
+                {
+                    undefined = false;
+                    isSolvedNum = false;
+                    nums.Clear();
+                    ops.Clear();
+                    return "Error";
+                }
+                SolveAddSub();
+                answerNum = nums[0];
+                nums.Clear();
+                ops.Clear();
+                return Convert.ToString(answerNum);
+            }
+            // checks and handles if a number has been clicked, but an operator has not been clicked
+            if (ops.Count == 0)
+            {
+                return currentNum;
+            }
+            // checks and handles if = operator is clicked after an operator and a number has not been clicked
+            if (currentNum == "")
+            {
+                ops.RemoveAt(ops.Count - 1);
+            }
+            else
+            {
+                lastNum = Convert.ToDecimal(currentNum);
+                AddNewNum();
+            }
+            SolveExponent();
+            SolveMultiply();
+            SolveDivide();
+            // checks and handles if user tries to divide by 0
+            if (undefined)
+            {
+                // resets values and undefined value and returns the error message
+                undefined = false;
+                nums.Clear();
+                ops.Clear();
+                return "Error";
+            }
+            SolveAddSub();
+            isSolvedNum = true;
+            answerNum = nums[0];
+            currentNum = "";
+            nums.Clear();
+            ops.Clear();
+            return Convert.ToString(answerNum);
+        }
+        public void SolveExponent()
+        {
+            while (ops.Contains('^'))
+            {
+                tempIndex = ops.IndexOf('^');
+
+                tempNum = (decimal)Math.Pow((double)nums[tempIndex], (double)nums[tempIndex + 1]);
+
+                nums.RemoveAt(tempIndex);
+                nums.RemoveAt(tempIndex);
+                nums.Insert(tempIndex, tempNum);
+
+                ops.RemoveAt(tempIndex);
+            }
+        }
+        public void SolveMultiply()
+        {
+            while (ops.Contains('*'))
+            {
+                tempIndex = ops.IndexOf('*');
+                tempNum = nums[tempIndex] * nums[tempIndex + 1];
+
+                nums.RemoveAt(tempIndex);
+                nums.RemoveAt(tempIndex);
+                nums.Insert(tempIndex, tempNum);
+
+                ops.RemoveAt(tempIndex);
+            }
+        }
+        public void SolveDivide()
+        {
+            while (ops.Contains('/'))
+            {
+                tempIndex = ops.IndexOf('/');
+                // error exception for undefined answer due to dividing by 0
+                if (nums[tempIndex + 1] == 0)
+                {
+                    undefined = true;
+                    break;
+                }
+                tempNum = nums[tempIndex] / nums[tempIndex + 1];
+
+                nums.RemoveAt(tempIndex);
+                nums.RemoveAt(tempIndex);
+                nums.Insert(tempIndex, tempNum);
+
+                ops.RemoveAt(tempIndex);
+            }
+        }
+        public void SolveAddSub()
+        {
+            while (ops.Contains('+') || ops.Contains('-'))
+            {
+                if (ops[0] == '+')
+                {
+                    tempNum = nums[0] + nums[1];
+                }
+                else if (ops[0] == '-')
+                {
+                    tempNum = nums[0] - nums[1];
+                }
+                else
+                {
+                    System.Console.WriteLine("Invalid Operator");
+                }
+
+                nums.RemoveAt(0);
+                nums.RemoveAt(0);
+                nums.Insert(0, tempNum);
+
+                ops.RemoveAt(0);
+            }
+        }
+
+
+        // * variables used in Equation class
+        List<decimal> nums;
+        List<char> ops;
+        string currentNum = "";
+        decimal tempNum;
+        int tempIndex;
+        decimal answerNum;
+        char lastOp;
+        decimal lastNum;
+        bool isSolvedNum = false;
+        bool undefined = false;
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Equation equation = new Equation();
+
+        public void UpdateTextBox(string str, TextBox tb)
+        {
+            tb.Text = str;
+        }
+        private void Btn0_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("0");
+        }
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("1");
+        }
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("2");
+        }
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("3");
+        }
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("4");
+        }
+        private void Btn5_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("5");
+        }
+        private void Btn6_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("6");
+        }
+        private void Btn7_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("7");
+        }
+        private void Btn8_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("8");
+        }
+        private void Btn9_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.NumClick("9");
+        }
+
+        private void BtnDec_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.DecClick();
+        }
+
+        private void BtnExp_Click(object sender, RoutedEventArgs e)
+        {
+            equation.OpClick('^');
+            txtBox.Text = "0";
+        }
+        private void BtnMult_Click(object sender, RoutedEventArgs e)
+        {
+            equation.OpClick('*');
+            txtBox.Text = "0";
+        }
+        private void BtnDiv_Click(object sender, RoutedEventArgs e)
+        {
+            equation.OpClick('/');
+            txtBox.Text = "0";
+        }
+        private void BtnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            equation.OpClick('+');
+            txtBox.Text = "0";
+        }
+        private void BtnMin_Click(object sender, RoutedEventArgs e)
+        {
+            equation.OpClick('-');
+            txtBox.Text = "0";
+        }
+
+        private void BtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.Solve();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.BackClick();
+        }
+
+        private void BtnC_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.ClearClick();
+        }
+
+        private void BtnCE_Click(object sender, RoutedEventArgs e)
+        {
+            equation.ClearAllClick();
+            txtBox.Text = "0";
+        }
+
+        private void BtnPosNeg_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = equation.PosNegClick();
+        }
+    }
+}
